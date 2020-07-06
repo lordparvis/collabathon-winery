3d0ae3942e55a7a0fc363048f68e9325e92f2405
aliciaroeck16 <aliciamata00@gmail.com>
2020-07-06 15:18:13 -0500

    creates dash path

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-        return HttpResponse('<h1>Hello</h1>')
+    return HttpResponse('<h1>Hello</h1>')
+
+def dashboard(request):
+    return HttpResponse('<h1>Dashboard</h1>')
